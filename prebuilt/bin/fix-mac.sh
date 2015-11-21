8f0d07e842dce5c82f53ee0f1f70dd92562f84b3
HashBang <hashbang173@gmail.com>
2015-11-21 12:00:35 -0500

    make fix-mac.sh compatible with otterx
    Change-Id: Ib762ea7edd6a4dceec1962fabac200b29f34015f

--- prebuilt/bin/fix-mac.sh
+++ prebuilt/bin/fix-mac.sh
@@ -5,22 +5,20 @@
 ROM_NVS=/system/etc/firmware/ti-connectivity/wl1271-nvs_127x.bin
 ORIG_NVS=/data/misc/wifi/wl1271-nvs.bin.orig
 NEW_NVS=/data/misc/wifi/wl1271-nvs.bin
-MACADDR_FILE=/rom/devconf/MACAddress
+MACADDR=$(getprop ro.boot.wifimac)
 MACADDR_COPY=/data/misc/wifi/MACAddress
 
 PATH=/vendor/bin:/system/bin:/system/xbin
 umask 0022
 
+[ $MACADDR ] || exit 1
+
 # Don't bother updating the nvs file if the one shipped in the ROM hasn't
 # changed since the last boot and the MAC address of the device hasn't changed
 cmp "$ROM_NVS" "$ORIG_NVS" > /dev/null 2>&1 && \
-    cmp "$MACADDR_FILE" "$MACADDR_COPY" > /dev/null 2>&1 && \
+    echo $MACADDR | cmp "$MACADDR_COPY" > /dev/null 2>&1 && \
     insmod /system/lib/modules/wl12xx_sdio.ko && \
     exit 0
-
-# Get the MAC address
-macaddr=$(cat "$MACADDR_FILE")
-[ $macaddr ] || exit 1
 
 # The MAC address is stored in the nvs file in two pieces: the four
 # least-significant bytes in little-endian order starting at byte offset 3
@@ -29,8 +27,8 @@ macaddr=$(cat "$MACADDR_FILE")
 #
 # We're using printf to write these bytes to the file, so parse the MAC
 # address to produce the escape sequences we'll use as arguments to printf.
-lowbytes=$(echo "$macaddr" | sed -e 's#^\(..\)\(..\)\(..\)\(..\)\(..\)\(..\)$#\\x\6\\x\5\\x\4\\x\3#')
-highbytes=$(echo "$macaddr" | sed -e 's#^\(..\)\(..\)\(..\)\(..\)\(..\)\(..\)$#\\x\2\\x\1#')
+lowbytes=$(echo "$MACADDR" | sed -e 's#^\(..\)\(..\)\(..\)\(..\)\(..\)\(..\)$#\\x\6\\x\5\\x\4\\x\3#')
+highbytes=$(echo "$MACADDR" | sed -e 's#^\(..\)\(..\)\(..\)\(..\)\(..\)\(..\)$#\\x\2\\x\1#')
 
 # Create the new nvs file by copying over the ROM's copy byte by byte,
 # replacing only the pieces containing the MAC address
@@ -46,7 +44,7 @@ cp "$ROM_NVS" "$ORIG_NVS"
 # Also store the MAC address referenced in the NVS, so that we can detect the
 # case where this installation is cloned/moved to another device and update
 # the NVS file accordingly
-cp "$MACADDR_FILE" "$MACADDR_COPY"
+echo "$MACADDR" > "$MACADDR_COPY"
 
 insmod /system/lib/modules/wl12xx_sdio.ko
 
